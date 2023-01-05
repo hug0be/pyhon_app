4fe400392688abcf19167aed0037e699c8670a7e
ronanter <ronan1terras@gmail.com>
2023-01-05 15:37:53 +0100

    coucou

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class MainWindow(QMainWindow):
@@ -10,8 +10,17 @@ if __name__ == "__main__":
     # On crée l'instance d'application en lui passant le tableau des arguments.
     app = QApplication(sys.argv)
 
-    # TODO : Instancier et afficher votre fenêtre graphique.
+
+    def say_hello():
+        print("Button clicked, Hello!")
+
+
+
+        # TODO : Instancier et afficher votre fenêtre graphique.
     window = MainWindow()
+    button = QPushButton("Click me", window)
+    button.clicked.connect(say_hello)
+    button.show()
     window.show()
 
     # On démarre la boucle de gestion des événements. coucou
