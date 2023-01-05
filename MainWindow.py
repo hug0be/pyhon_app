76773a313df9502d9a3473dcb21ff8fc5f6edba6
ronanter <ronan1terras@gmail.com>
2023-01-05 14:31:44 +0100

    coucou

--- MainWindow.py
+++ MainWindow.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
 
-    # On démarre la boucle de gestion des événements.
+    # On démarre la boucle de gestion des événements. coucou
     sys.exit(app.exec())
