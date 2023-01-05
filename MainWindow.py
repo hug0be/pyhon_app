a936909aa04fbefeeafd62ebcf2118d8aaff1a18
hug0be <83722580+hug0be@users.noreply.github.com>
2023-01-05 15:07:45 +0100

    Ajout du titre et favicon

--- MainWindow.py
+++ MainWindow.py
@@ -1,10 +1,14 @@
 import sys
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-
 class MainWindow(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("WinQuest")
+        self.setWindowIcon(QIcon("resources/images/favicon.png"))
+        self.resize(800, 600)
 
 if __name__ == "__main__":
     # On crée l'instance d'application en lui passant le tableau des arguments.
@@ -14,5 +18,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
 
-    # On démarre la boucle de gestion des événements. coucou
+    # On démarre la boucle de gestion des événements.
     sys.exit(app.exec())
