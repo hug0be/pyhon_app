adf892e54c8dfa7e630ddf37aebebd085d3efa61
hug0be <83722580+hug0be@users.noreply.github.com>
2023-01-05 17:33:18 +0100

    Ajout du favicon, et des deux boutons "se connecter" et "créer un compte"

--- MainWindow.py
+++ MainWindow.py
@@ -2,16 +2,14 @@ import sys
 
 from PySide6 import QtUiTools
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtUiTools import QUiLoader
 
 
 loader = QUiLoader()
 
 if __name__ == "__main__":
-    # On crée l'instance d'application en lui passant le tableau des arguments.
     app = QApplication(sys.argv)
-
 
     def say_hello():
         print("Button clicked, Hello!")
@@ -19,7 +17,8 @@ if __name__ == "__main__":
     def clickclick():
         print("clickclick")
 
-    window = loader.load("untitled.ui", None)
+    window: QMainWindow = loader.load("untitled.ui", None)
+    window.setWindowIcon(QIcon('resources/images/favicon_96x96.png'))
     loader = QtUiTools.QUiLoader()
     window.pushButton_5.clicked.connect(clickclick)
 
